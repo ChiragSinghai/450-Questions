a33b3489915678d1b48f127f9a8d3fd44b7519e9
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-11-24 16:52:30 +0530

    function

--- Chirag/Array/Que. 8 minimise difference in heights.py
+++ Chirag/Array/Que. 8 minimise difference in heights.py
@@ -25,6 +25,72 @@ def solution(arr,n,k):
     print(ans)
 
 
+# Python 3 program to find the minimum 
+# possible difference between maximum 
+# and minimum elements when we have to 
+# add / subtract every number by k 
+
+# Modifies the array by subtracting / 
+# adding k to every element such that 
+# the difference between maximum and 
+# minimum is minimized 
+def getMinDiff(arr, n, k): 
+
+	if (n == 1): 
+		return 0
+
+	# Sort all elements 
+	arr.sort()
+	print(arr)
+
+	# Initialize result 
+	ans = arr[n-1] - arr[0] 
+
+	# Handle corner elements 
+	small = arr[0] + k 
+	big = arr[n-1] - k 
+	
+	if (small > big): 
+		small, big = big, small 
+
+	# Traverse middle elements 
+	for i in range(1, n-1): 
+	
+		subtract = arr[i] - k 
+		add = arr[i] + k 
+
+		# If both subtraction and addition 
+		# do not change diff 
+		if (subtract >= small or add <= big): 
+			continue
+
+		# Either subtraction causes a smaller 
+		# number or addition causes a greater 
+		# number. Update small or big using 
+		# greedy approach (If big - subtract 
+		# causes smaller diff, update small 
+		# Else update big) 
+		if (big - subtract <= add - small): 
+			small = subtract 
+		else: 
+			big = add 
+	
+
+	return min(ans, big - small) 
+
+
+# Driver function 
+arr =[6,1,9,1,1,7,9,5,2,10]
+ 
+n = len(arr) 
+k = 4
+
+print("Maximum difference is", getMinDiff(arr, n, k)) 
+
+# This code is contributed by 
+# Smitha Dinesh Semwal 
+
+'''
 for _ in range(int(input())):
     #k=int(input())
     #A=list(map(int,input().split()))
@@ -32,3 +98,4 @@ for _ in range(int(input())):
     A=[6,1,9,1,1,7,9,5,2,10]
 
     solution(A,len(A),k)
+'''
