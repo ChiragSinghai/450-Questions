3d16ff3e2265ef1b64a966d4c1032de6a850e6ca
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-11-29 22:56:53 +0530

    Update merge intervals.py

--- Chirag/Array/merge intervals.py
+++ Chirag/Array/merge intervals.py
@@ -2,7 +2,7 @@ def merger(A):
     A=sorted(A,key=lambda x:x[0])
     ans=[A[0]]
     for i in range(1,len(A)):
-        print(ans)
+        #print(ans)
         if A[i][0]<=ans[-1][1]:
             ans[-1][0]=min(A[i][0],ans[-1][0])
             ans[-1][1]=max(A[i][1],ans[-1][1])
