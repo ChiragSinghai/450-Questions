7c4fc38b8f80e90849261c552fa514913807bc93
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-11-30 18:23:50 +0530

    Update best time to buy and sell stock.py

--- Chirag/Array/best time to buy and sell stock.py
+++ Chirag/Array/best time to buy and sell stock.py
@@ -1,11 +1,15 @@
 def fun(prices):
     mi=ma=prices[0]
+    maximum=0
     for i in prices:
         if mi>i:
-            mi=i
-        else:
-            if i>ma:
-                ma=i
-    print(ma-mi if ma-mi>0 else 0)
+            maximum=max(maximum,ma-mi)
+            mi=ma=i
+        if ma<i:
+            ma=i
+    print(maximum)
+
 if __name__=='__main__':
-    fun([7,1,5,3,6,4])
+    #A=[7,1,5,3,6,4]
+    arr=list(map(int,input().split()))
+    fun(arr)
