fd2b1440a9bcf40fd744678d22de64fd0aebbd8d
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-01 18:38:42 +0530

    Update pairs having sum equal to given number.py

--- Chirag/Array/pairs having sum equal to given number.py
+++ Chirag/Array/pairs having sum equal to given number.py
@@ -7,12 +7,23 @@ def bruteforceApproach(A,n,s):
                 count+=1
             j+=1
     print(count)
+#i dont understand this solution but because I found array of 1000 digits for
+#small arrays is quite non optimal
 def optimalSolution(A,n,s):
-    pass
+    B=[0]*1000
+    for i in A:
+        B[i]+=1
+    count=0
+    for i in A:
+        count+=B[s-i]
+        if (s - i ==i):
+            twice_count-=1
         
+    print(count//2)
 if __name__=='__main__':
     N=int(input())
     A=list(map(int,input().split()))
     S=int(input())
     bruteforceApproach(A,N,S)
+    optimalSolution(A,N,S)
         
