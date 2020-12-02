dd329374fcb1cdca4a10cdd2e021e76e8dcb5763
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-02 11:18:24 +0530

    Update common element in 3 sorted array.py

--- Chirag/Array/common element in 3 sorted array.py
+++ Chirag/Array/common element in 3 sorted array.py
@@ -1,1 +1,24 @@
+def search(A,B,C,x,y,z):
+    res=[]
+    i=j=k=0
+    
+    while i < x and j < y and k < z:
+        if A[i]==B[j]==C[k]:
+            res.append(A[i])
+            i+=1
+            j+=1
+            k+=1
+        elif min(A[i],B[j],C[k])==A[i]:
+            i+=1
+        elif min(A[i],B[j],C[k])==B[j]:
+            j+=1
+        else:
+            k+=1
+    return res
+    
+
 if __name__=='__main__':
+    A=[4,5,6,8,9]
+    B=[1,2,4,6,10]
+    C=[0,3,4,6]
+    print(search(A,B,C,len(A),len(B),len(C)))
