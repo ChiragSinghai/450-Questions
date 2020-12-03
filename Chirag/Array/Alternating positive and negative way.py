b9b73d716dc87efff268756b6f9bf8cbb17b24fa
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-03 11:32:00 +0530

    Update Alternating positive and negative way.py

--- Chirag/Array/Alternating positive and negative way.py
+++ Chirag/Array/Alternating positive and negative way.py
@@ -3,8 +3,14 @@ def arrange(A,n):
     for i in range(n):
         if check>=0:
             if (A[i]>=0 and A[check]<0) and (A[i]<0 and A[check]>=0):
-                
+                rotate(A,n,i,check)
+                if i-check>2:
+                    check+=2
+                else:
+                    check-=1
         else:
             if (A[i]>=0 and i%2==0) or (A[i]<0 and i%2==1):
                 check=i
         
+def rotate(A,n,i,check):
+    
