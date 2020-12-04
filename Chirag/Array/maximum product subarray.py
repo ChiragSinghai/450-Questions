2b8f3c7621705f221d8165ffb138617d4415e946
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-04 13:13:15 +0530

    Update maximum product subarray.py

--- Chirag/Array/maximum product subarray.py
+++ Chirag/Array/maximum product subarray.py
@@ -9,6 +9,14 @@ def find(A,n):
         minval=max(A[i],minval*A[i])
         maxprod=max(maxprod,maxval)
     return maxprod
-        
+def brutual_force(A,n):
+    m=A[0]
+    for i in range(n):
+        prod=A[i]
+        for j in range(i+1,n): 
+            prod=prod*A[j]
+        m=max(m,prod)
+    return m
 if __name__=='__main__':
     print(find( [-1, -3, -10, 0, 60],5))
+    print(brutual_force( [-1, -3, -10, 0, 60],5))
