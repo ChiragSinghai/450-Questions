be52ae9bffc8801802e78636c9fba7557157b5db
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-04 13:02:26 +0530

    Update maximum product subarray.py

--- Chirag/Array/maximum product subarray.py
+++ Chirag/Array/maximum product subarray.py
@@ -1,5 +1,14 @@
 def find(A,n):
+    minval=A[0]
+    maxval=A[0]
     maxprod=A[0]
-    curmax=maxprod
+    for i in range(1,n):
+        if A[i]<0:
+            maxval,minval=minval,maxval
+        maxval=max(A[i],maxval*A[i])
+        minval=max(A[i],minval*A[i])
+        maxprod=max(maxprod,maxval)
+    return maxprod
+        
 if __name__=='__main__':
-    pass
+    print(find( [-1, -3, -10, 0, 60],5))
