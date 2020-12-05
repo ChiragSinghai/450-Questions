6944e624d0d89948c150862339f7a8002e266056
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-05 17:21:42 +0530

    Update largest subsequence integer.py

--- Chirag/Array/largest subsequence integer.py
+++ Chirag/Array/largest subsequence integer.py
@@ -1,2 +1,14 @@
+def findSub(A,n):
+    A.sort()
+    l=[]
+    count=1
+    ans=0
+    for i in range(1,n):
+        if A[i]==A[i-1]-1 or A[i]==A[i-1]:
+            count+=1
+        else:
+            count=1
+        ans = max(ans, count)
+    print(count)
 if __name__=='__main__':
-    pass
+    findSub([6,2,7,3,4,5,31,31,5]
