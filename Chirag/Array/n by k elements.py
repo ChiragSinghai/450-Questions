8a928169d1c9a476c70d5eaf73fb50e22f79ce45
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-05 22:22:47 +0530

    Update n by k elements.py

--- Chirag/Array/n by k elements.py
+++ Chirag/Array/n by k elements.py
@@ -9,7 +9,26 @@ def fun(A,n,k):
     for i in d:
         if d[i]>=m:
             print(i)
+
+def anotherapproach(A,n,k):
+    A.sort()
+    count=1
+    m=n//k
+    i=1
+    check=True
+    while i<n:
+        if A[i]==A[i-1]:
+            count+=1
+            if count>=m and check:
+                check=False
+                print(A[i])
+        else:
+            check=True
+            count=1
+        i+=1
+        
 if __name__=='__main__':
-    fun([1,2,2,3,4,4,4,4],8,4)
+    fun([1,2,2,2,3,3,4,4,4,4],8,4)
+    anotherapproach([1,2,2,3,4,4,4,4],8,4)
 
         
