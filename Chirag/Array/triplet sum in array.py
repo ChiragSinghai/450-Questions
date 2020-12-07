3b8f050547c6b15907cf1ce781b4842581d64552
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-07 11:13:35 +0530

    Update triplet sum in array.py

--- Chirag/Array/triplet sum in array.py
+++ Chirag/Array/triplet sum in array.py
@@ -1,3 +1,11 @@
+def findtriplet(A,s):
+    n=len(A)
+    A.sort()
+    for i in range(0,n-2):
+        l=i+1
+        r=n-1
+    while l<r:
+        if A[i]+A[l]+A[r]
 if __name__=='__main__':
     A=input().split()
     s=int(input())
