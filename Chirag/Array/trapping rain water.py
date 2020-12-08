e81e818c0b504d5b561e27fa57e83eaa45c19e24
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-08 18:19:28 +0530

    Update trapping rain water.py

--- Chirag/Array/trapping rain water.py
+++ Chirag/Array/trapping rain water.py
@@ -1,5 +1,22 @@
 def trap(A):
     n=len(A)
+    lo=0
+    hi=n-1
+    mi=ans=ma=0
+    while lo<=hi:
+        if A[lo]<A[hi]:
+            if A[lo]>mi:
+                mi=A[lo]
+            else:
+                ans+=mi-A[lo]
+            lo+=1
+        else:
+            if A[hi]>ma:
+                ma=A[hi]
+            else:
+                ans+=ma-A[hi]
+            hi-=1
+    print(ans)
     
 if __name__=='__main__':
-    trap(A)
+    trap([7,4,0,9])
