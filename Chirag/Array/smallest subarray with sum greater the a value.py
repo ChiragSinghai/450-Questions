6abc3b390b31ab24874e3bf9ec24dde035165e7a
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-09 10:49:17 +0530

    Update smallest subarray with sum greater the a value.py

--- Chirag/Array/smallest subarray with sum greater the a value.py
+++ Chirag/Array/smallest subarray with sum greater the a value.py
@@ -1,7 +1,19 @@
-def getsubarray(A):
+def getsubarray(A,s):
     n=len(A)
-    for i in range(n):
-        pass
+    start=end=0
+    cur_sum=0
+    min_len=n+1
+    while end<n:
+        while cur_sum<=s and end<n:
+            cur_sum+=A[end]
+            end+=1
+        while start<n and cur_sum>s:
+            if end-start<min_len:
+                min_len=end-start
+            cur_sum-=A[start]
+            start+=1
+    print(min_len)
+        
 if __name__=='__main__':
-    getsubarray(A)
+    getsubarray([1, 10, 5, 2, 7]  ,9)
     
