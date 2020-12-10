581be79253e9c49972ee10fa34da948e5d1e52f7
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-10 10:37:12 +0530

    implemented separation

---
+++ Chirag/Array/separate 0,1 and 2 from a array.py
@@ -0,0 +1,22 @@
+def sorting(A):
+    n=len(A)
+    start=0
+    end=n-1
+    mid=0
+    while mid<=end:
+        if A[mid]==0:
+            A[start],A[mid]=A[mid],A[start]
+            start+=1
+            mid+=1
+        elif A[mid]==1:
+            A[mid],A[mid]=A[mid],A[mid]
+            mid+=1
+        else:
+            A[end],A[mid]=A[mid],A[end]
+            end-=1
+    print(A)
+
+
+            
+if __name__=='__main__':
+    sorting([1,2,1,2,0,0,0,2,1,1,2,0])
