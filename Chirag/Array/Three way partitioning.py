57238d4dc97fb22e6a8d5aeccc6192bee4b1f379
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-10 10:58:47 +0530

    Update Three way partitioning.py

--- Chirag/Array/Three way partitioning.py
+++ Chirag/Array/Three way partitioning.py
@@ -1,6 +1,14 @@
 def part(A,s):
-    pass
+    high=s[1]
+    low=s[1]
+    n=len(A)
+    start=0
+    end=n-1
+    mid=0
+    
+
 def brutual(A,s):
     A.sort()
 if __name__=='__main__':
     part(A,s)
+    #brutual
