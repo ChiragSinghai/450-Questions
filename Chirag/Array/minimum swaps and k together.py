b24e3fabca17db29c5ab3f5229a3e8485fffb48c
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-10 12:55:09 +0530

    Update minimum swaps and k together.py

--- Chirag/Array/minimum swaps and k together.py
+++ Chirag/Array/minimum swaps and k together.py
@@ -1,2 +1,24 @@
+def partition5(A,n,k):
+    start=0
+    end=n-1
+    count=0
+    index=-1
+    subcount=0
+    for i in A:
+        if i<=k:
+            count+=1
+            subcount+=1
+        else:
+            count=0
+        index=max(index,count)
+    print(subcount-count)
+        
+            
+    
+    
+
 if __name__=='__main__':
-    swap()
+    n=int(input())
+    A=list(map(int,input().split()))
+    k=int(input())
+    partition5(A,n,k)
