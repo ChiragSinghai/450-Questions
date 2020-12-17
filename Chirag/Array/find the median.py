46fd3f7f9e7ba65273db0e5be89a4646cd37cbf6
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-17 11:48:28 +0530

    Create find the median.py

---
+++ Chirag/Array/find the median.py
@@ -0,0 +1,19 @@
+def solution(A):
+    n=len(A)
+    index=n//2
+    if n==1:
+        return A[0]
+    if n==2:
+        return (A[0]+A[1])//2
+    for i in range(index+1):
+        m=i+1
+        for j in range(m+1,n):
+            if A[m]>A[j]:
+                m=j
+        if A[m]<A[i]:
+            A[i],A[m]=A[m],A[i]
+	if n%2==0:
+	    return (A[index]+A[index-1])//2
+	else:
+            return A[index]
+        
