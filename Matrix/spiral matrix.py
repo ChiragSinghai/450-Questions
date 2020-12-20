69952a7df90391d8ee80f38c8685be7611949c8a
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-20 18:11:02 +0530

    Update spiral matrix.py

--- Matrix/spiral matrix.py
+++ Matrix/spiral matrix.py
@@ -11,17 +11,19 @@ def spiral(L):
         for i in range(row,r):
             print(L[i][c-1],end=' ')
         c-=1
-        for i in range(c-1,column-1,-1):
-            print(L[r-1][i],end=' ')
-        r-=1
-        for i in range(r-1,row-1,-1):
-            print(L[i][column],end=' ')
-        column+=1
+        if row<r:
+            for i in range(c-1,column-1,-1):
+                print(L[r-1][i],end=' ')
+            r-=1
+        if column<c:
+            for i in range(r-1,row-1,-1):
+                print(L[i][column],end=' ')
+            column+=1
             
 if __name__=='__main__':
     L=[[1,2,3,4,5],
-       [14,15,16,17,6]]
-       #[13,20,19,18,7],
+       [14,15,16,17,6],
+       [13,20,19,18,7]]
        #[12,11,10,9,8]]
        
     spiral(L)
