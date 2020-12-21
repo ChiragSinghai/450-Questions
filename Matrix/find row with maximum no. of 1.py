40d0b48079c40e09297407083d4b978417521f3e
ChiragSinghai <73294794+ChiragSinghai@users.noreply.github.com>
2020-12-21 20:33:50 +0530

    Update find row with maximum no. of 1.py

--- Matrix/find row with maximum no. of 1.py
+++ Matrix/find row with maximum no. of 1.py
@@ -1,6 +1,5 @@
 class Solution:
     def binary(self,arr,m):
-        
         i=0
         while i<=m:
             mid=(i+m)//2
@@ -19,9 +18,31 @@ class Solution:
         ma=self.binary(arr[0],m-1)
         #print(ma)
         for i in range(1,n):
-            if arr[i][ma]==1 & ma!=-1:
+            if arr[i][ma]==1 and ma!=-1:
                 index=self.binary(arr[i],ma)
                 #print(index,row,i)
+                if index!=-1 and index<ma:
+                    ma=index
+                    row=i
+            if ma==-1:
+                ma=self.binary(arr[i],m-1)
+                row=i
+        if ma==-1:
+            return -1 
+        return row
+    def binary1(self,arr,m):
+        for i in range(m,-1,-1):
+            if arr[i]==0:
+                return i
+        return -1 
+    
+    def rowWithMax1s1(self,arr, n, m):
+        row=0
+        ma=self.binary(arr[0],m-1)
+        #print(ma)
+        for i in range(1,n):
+            if arr[i][ma]==1 and ma!=-1:
+                index=self.binary(arr[i],ma)
                 if index!=-1 and index<ma:
                     ma=index
                     row=i
@@ -37,8 +58,8 @@ if __name__=='__main__':
     n=5
     m=4
     obj=Solution()
-    obj.rowWithMax1s(arr,n,m)
-
+    print(obj.rowWithMax1s(arr,n,m))
+    print(obj.binary(arr[4],3))
 
 
     
