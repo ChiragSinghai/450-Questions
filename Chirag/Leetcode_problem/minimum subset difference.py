f728a8b142c695088ff2e26386184735027d641e
ChiragSinghai <jainchirag361@gmail.com>
2021-07-09 22:46:52 +0530

    Update minimum subset difference.py

--- Chirag/Leetcode_problem/minimum subset difference.py
+++ Chirag/Leetcode_problem/minimum subset difference.py
@@ -1,3 +1,17 @@
+def printmat(dp):
+    for row in dp:
+        print(row)
+        
+def findmin(A,n,S):
+    dp=[[False for i in range(S+1)]for i in range(n+1)]
+    for i in range(n+1):
+        dp[i][0]=True
+    printmat(dp)
+    
+    
+
 if __name__=='__main__':
     A=[4,3,1,7]
-    
+    n=len(A)
+    S=4
+    findmin(A,n,S)
