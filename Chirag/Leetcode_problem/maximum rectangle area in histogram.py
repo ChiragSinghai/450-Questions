9833ed76d1a77c5702190728bb8034fc1d6846b5
ChiragSinghai <jainchirag361@gmail.com>
2021-07-24 12:24:17 +0530

    Update maximum rectangle area in histogram.py

--- Chirag/Leetcode_problem/maximum rectangle area in histogram.py
+++ Chirag/Leetcode_problem/maximum rectangle area in histogram.py
@@ -37,7 +37,26 @@ def find(hist):
     for i in range(n):
         max_area = max(max_area,(right[i]-left[i]+1)*hist[i])
     print(max_area)
-        
+
+def findarea(A):
+    maxarea = 0
+    stack=[]
+    n=len(A)
+    for i in range(n):
+        lastwidth=n*n+1
+        while stack and A[stack[-1]]>A[i]:
+            lastwidth = stack[-1]
+            currarea = (i-stack.pop())*A[lastwidth]
+            maxarea=max(maxarea,currarea)
+            
+        if not stack or A[stack[-1]]<A[i]:
+            stack.append(min(lastwidth,i))
+        print(stack,maxarea)
+    while stack:
+        currarea = (n-1-stack[-1])*A[stack[-1]]
+        stack.pop()
+        maxarea=max(currarea,maxarea)
+    print(maxarea)
 if __name__=='__main__':
     A = [2,1,5,6,2,3]
-    find(A)
+    findarea(A)
