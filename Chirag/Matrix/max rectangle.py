6ca091e5ad0753b0015898827fb37b0a2f988aab
ChiragSinghai <jainchirag361@gmail.com>
2021-07-27 18:34:21 +0530

    Update max rectangle.py

--- Chirag/Matrix/max rectangle.py
+++ Chirag/Matrix/max rectangle.py
@@ -1,1 +1,14 @@
+def maxArea(row):
 
+def getmax(mat):
+    for row in mat:
+        pass
+        
+
+
+if __name__=='__main__':
+    A=[["1","0","1","0","0"],
+       ["1","0","1","1","1"],
+       ["1","1","1","1","1"],
+       ["1","0","0","1","0"]]
+    print(maxArea(row))
