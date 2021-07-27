9938346b518b3d3bf74a89feb316bf77124a7afb
ChiragSinghai <jainchirag361@gmail.com>
2021-07-27 19:47:26 +0530

    Update missing number leetcode-268.py

--- Chirag/Leetcode_problem/missing number leetcode-268.py
+++ Chirag/Leetcode_problem/missing number leetcode-268.py
@@ -1,10 +1,12 @@
 def getnum(A):
     n=len(A)
+    number=0
     for i in range(n):
-        number = i^A[i]
+        number = number^i^A[i]
+    number^=n
     return number
 
 
 if __name__=='__main__':
-    A=[3,2,4,1]
+    A=[3,2,1,4]
     print(getnum(A))
