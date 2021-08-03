09d166821a4231c31ef070ad0ce669c30bf7141b
ChiragSinghai <jainchirag361@gmail.com>
2021-08-03 11:55:07 +0530

    Update permutation I leetcode 46.py

--- Chirag/Leetcode_problem/permutation I leetcode 46.py
+++ Chirag/Leetcode_problem/permutation I leetcode 46.py
@@ -16,5 +16,23 @@ def permute(L):
     return result
 
 
-A=[1,1,3]
+A=[1,2,3]
 print(permute(A))
+
+def to_string(A):
+    return ''.join(A)
+
+def permute(A,l,r):
+    if l==r:
+        print(to_string(A))
+    else:
+        for i in range(l,r+1):
+            A[l],A[i]=A[i],A[l]
+            permute(A,l+1,r)
+            A[l],A[i]=A[i],A[l]
+
+A='ABC'
+A=list(A)
+n=len(A)-1
+permute(A,0,n)
+    
