71507929dc653b9d33537a84f98e5820ac7fe84c
ChiragSinghai <jainchirag361@gmail.com>
2021-08-09 16:21:09 +0530

    Create 767. Reorganize String.py

---
+++ Chirag/Leetcode_problem/767. Reorganize String.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        D={}
+        for i in s:
+            if i not in D:
+                D[i]=0
+            D[i]+=1
+        heap=[]
+        for i,j in D.items():
+            heappush(heap,(-j,i))
+        
+        res = ""
+        prev_freq, prev_ch = heapq.heappop(heap)
+        res += prev_ch
+        while heap:
+            curr_freq, curr_ch = heapq.heappop(heap)
+            res += curr_ch
+            if prev_freq < -1: 
+                heapq.heappush(heap, (prev_freq + 1, prev_ch))
+            prev_freq, prev_ch = curr_freq, curr_ch
+            
+        if len(s) != len(res):
+            return ""
+        return res
+                
