6e7d53834722779bcd5d1ddd40f852536e317871
ChiragSinghai <jainchirag361@gmail.com>
2021-08-09 22:29:01 +0530

    Update 720. Longest Word in Dictionary.py

--- Chirag/Leetcode_problem/720. Longest Word in Dictionary.py
+++ Chirag/Leetcode_problem/720. Longest Word in Dictionary.py
@@ -28,3 +28,41 @@ class Solution:
             elif len(new)==len(previous):
                 previous=min(new,previous)
         return previous
+
+#another approach
+    
+'''
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        D={}
+        L=[0]*31
+        maxlen=0
+        for word in words:
+            l=len(word)
+            maxlen=max(l,maxlen)
+            if l not in D:
+                D[l]=set()
+            if L[l]==0:
+                L[l]=[word]
+                continue
+            L[l].append(word)
+        #print(D)
+        #print(L)
+        if L[1]==0:
+            return ''
+        D[1]=set(L[1])
+        
+        i=2
+        while i<=maxlen:
+            if L[i]==0:
+                break
+            for item in L[i]:
+                if item[0:i-1] in D[i-1]:
+                    D[i].add(item)
+            
+            i+=1
+        while maxlen:
+            if maxlen in D and D[maxlen]:
+                return sorted(D[maxlen])[0]
+            maxlen-=1
+'''
