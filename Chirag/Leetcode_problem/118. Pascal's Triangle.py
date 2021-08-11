3d28c765ccbe8f6afc351bc37de8db0af6e570fe
ChiragSinghai <jainchirag361@gmail.com>
2021-08-11 16:57:43 +0530

    Update 118. Pascal's Triangle.py

--- Chirag/Leetcode_problem/118. Pascal's Triangle.py
+++ Chirag/Leetcode_problem/118. Pascal's Triangle.py
@@ -1,3 +1,7 @@
+def printmat(dp):
+    for row in dp:
+        print(row)
+
 def generate(numRows):
         matrix=[[1]]
         for i in range(1,numRows):
@@ -6,7 +10,15 @@ def generate(numRows):
                 temp[j]=temp[j]+temp[j+1]
             matrix.append(temp[:i+1])
         return matrix
-def printmat(dp):
-    for row in dp:
-        print(row)
-printmat(generate(5))
+
+def generate1(numRows):
+        if numRows==1:
+            return [[1]]
+        matrix=[[1],[1,1]]
+        for i in range(2,numRows):
+            temp=[]
+            for j in range(i-1):
+                temp.append(matrix[i-1][j]+matrix[i-1][j+1])
+            matrix.append([1]+temp+[1])
+        return matrix
+printmat(generate1(5))
