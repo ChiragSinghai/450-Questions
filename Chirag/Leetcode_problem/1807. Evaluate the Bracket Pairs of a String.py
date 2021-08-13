08a89ba36f82f2d6be2b22a7d381bcd712d199fb
ChiragSinghai <jainchirag361@gmail.com>
2021-08-13 18:23:07 +0530

    Update 1807. Evaluate the Bracket Pairs of a String.py

--- Chirag/Leetcode_problem/1807. Evaluate the Bracket Pairs of a String.py
+++ Chirag/Leetcode_problem/1807. Evaluate the Bracket Pairs of a String.py
@@ -19,6 +19,45 @@ def evaluate1(s,knowledge):
             result += graph.get(key, '?')
             stack.clear()
     return result
+def evaluate2(s,knowledge):    
+    D={k:v for k,v in knowledge}
+    while True:
+        matches=re.search(r'(\([a-z]*\))',s)
+        #print(s,matches)
+        if matches:
+            m='\('+matches.group()+'\)'
+            
+            
+            s=re.sub(m,D.get(s[matches.start()+1:matches.end()-1],'?'),s)
+        else:
+            return s
+def evaluate3(s,knowledge):
+    D={k:v for k,v in knowledge}
+    l=0
+    r=0
+    i=0
+    n=len(s)
+    res=''
+    found=False
+    
+
+    while i<n:
+        if s[i]=='(':
+            l=i
+            found=True
+        elif s[i]==')':
+            r=i
+            found=False
+        elif not found:
+            res+=s[i]
+        if r and l!=r:
+            #print(s[l+1:r])
+            res+=D.get(s[l+1:r],'?')
+            #del D[s[l+1:r]]
+            l=0
+            r=0
+        i+=1
+    return res
 
 print(evaluate1("(name)is(age)yearsold",[["name","bob"],["age","two"]]))
 
