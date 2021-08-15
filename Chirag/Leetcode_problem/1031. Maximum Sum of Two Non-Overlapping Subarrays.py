54298c606e9fd54a16c4247c11221f9c4e1a9660
ChiragSinghai <jainchirag361@gmail.com>
2021-08-15 11:36:19 +0530

    Update 1031. Maximum Sum of Two Non-Overlapping Subarrays.py

--- Chirag/Leetcode_problem/1031. Maximum Sum of Two Non-Overlapping Subarrays.py
+++ Chirag/Leetcode_problem/1031. Maximum Sum of Two Non-Overlapping Subarrays.py
@@ -44,6 +44,22 @@ def maxSumTwoNoOverlap(nums,x, y):
     for i in range(y-1,n-x):
         ans=max(ans,dp1[i]+dp2[i+1])
     return ans
+#faster approach
+def maxSumTwoNoOverlap1(A, L, M):
+    if len(A) < L + M: return 0
+    for i in range(1, len(A)):
+        A[i] += A[i - 1]
+    res, maxL, maxM = A[L + M - 1], A[L - 1], A[M - 1]
+    #print(M,L,maxL,maxM,res)
+    for i in range(L + M, len(A)):
+        #print(i,maxL,maxM,res)
+        maxL = max(maxL, A[i - M] - A[i - M - L])
+        #print(maxL,A[i-M],A[i-M-L])
+        maxM = max(maxM, A[i - L] - A[i - L - M])
+        #print(maxM,A[i-L],A[i-L-M])
+        res = max(res, maxL + A[i] - A[i - M], maxM + A[i] - A[i - L])
+        #print(res,A[i],A[i-M],A[i-L])
+    return res
 print(maxSumTwoNoOverlap([0,6,5,2,2,5,1,9,4],1,2))
-            
+print(maxSumTwoNoOverlap1([0,6,5,2,2,5,1,9,4],1,2))           
         
