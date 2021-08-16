bcecd88d46d8abca60bcc6da377aa6b1f5669e92
ChiragSinghai <jainchirag361@gmail.com>
2021-08-16 17:57:26 +0530

    Create 1352. Product of the Last K Numbers.py

---
+++ Chirag/Leetcode_problem/1352. Product of the Last K Numbers.py
@@ -0,0 +1,26 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.nums=[]
+        self.product=[]
+        self.zero=-1
+        self.count=0
+        
+    def add(self, num: int) -> None:
+        
+        self.nums.append(num)
+        if not num:
+            self.zero=max(self.zero,self.count)
+        self.count+=1
+        if not self.product:
+            self.product.append(num or 1)
+                    
+        else:
+            self.product.append((num*self.product[-1]) or 1) 
+            
+        
+    def getProduct(self, k: int) -> int:
+        if self.count-self.zero<=k:return 0
+        if k==len(self.nums):
+            return self.product[-1]
+        return self.product[-1]//self.product[-k-1]
