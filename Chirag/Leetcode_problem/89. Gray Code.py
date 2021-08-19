262ccf0048da3afdeeff1734234da6bc99eeb379
ChiragSinghai <jainchirag361@gmail.com>
2021-08-19 18:40:00 +0530

    Update 89. Gray Code.py

--- Chirag/Leetcode_problem/89. Gray Code.py
+++ Chirag/Leetcode_problem/89. Gray Code.py
@@ -12,5 +12,16 @@ class Solution:
     tab=gray()
     def grayCode(self, n):        
         return Solution.tab[n-1]
-obj=Solution()
+class Solution1:
+    def grayCode(self, n) :        
+        tab=[0]
+        x=0
+        while x<n:
+            b=1<<x
+            for i in range(b-1,-1,-1):
+                tab.append(b|tab[i])
+            x+=1
+        return tab
+    
+obj=Solution1()
 print(obj.grayCode(4))
