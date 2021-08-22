c9fa876df77f982a883c9adb0237e17fbc752b76
ChiragSinghai <jainchirag361@gmail.com>
2021-08-22 22:00:12 +0530

    Update Longest_common_subsequence.py

--- Chirag/Leetcode_problem/Longest_common_subsequence.py
+++ Chirag/Leetcode_problem/Longest_common_subsequence.py
@@ -15,7 +15,8 @@ def LCS(str1,str2):
                 dp[i][j]=max(dp[i-1][j],dp[i][j-1])
     printmat(dp)
     print(dp[m][n])
-    printLCS(dp,str1)
+    printLCS1(dp,str1,m,n,'')
+    #print(a)
 
 def printLCS(dp,str1):
     m=len(dp)-1
@@ -38,8 +39,28 @@ def printLCS(dp,str1):
         else:
             n-=1
     print(s[::-1])
+a=[]
+def printLCS1(dp,s1,m,n,res):
+    if m==0 or n==0:
+        a.append(res)
+        return 0
+    upValue=dp[m-1][n]
+    leftValue=dp[m][n-1]
+    diValue=dp[m-1][n-1]
+    if upValue==leftValue==diValue and dp[m][n]!=diValue:
+        printLCS1(dp,s1,m-1,n-1,res+s1[m-1])
+    else:
+        if leftValue==upValue:
+            printLCS1(dp,s1,m-1,n,res)
+            printLCS1(dp,s1,m,n-1,res)
+        elif leftValue<upValue:
+            printLCS1(dp,s1,m-1,n,res)
+        else:
+            printLCS1(dp,s1,m,n-1,res)
+
+    
 
 if __name__=='__main__':
     s1="GXTXAYB"
     s2="AGGTAB"
-    LCS(s1,s2)
+    LCS("ihlnqpdwqgcd","mgrumwmpjedv")
