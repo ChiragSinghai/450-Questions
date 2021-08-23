cb6bb1918e13eb58d4e103e75e39bd7aa08f1ea5
ChiragSinghai <jainchirag361@gmail.com>
2021-08-23 11:48:45 +0530

    Update 414. Third Maximum Number.py

--- Chirag/Leetcode_problem/414. Third Maximum Number.py
+++ Chirag/Leetcode_problem/414. Third Maximum Number.py
@@ -13,4 +13,20 @@ def thirdMax(nums):
         nums[curr],nums[len(nums)-1-i]=nums[len(nums)-1-i],nums[curr]
         #print(nums)
     return nums[-3]
+def thirdMax(self, nums: List[int]) -> int:
+    nums=list(set(nums))
+    if len(nums)<=2:
+        return max(nums)
+    t,s,f=sorted(nums[:3])
+    for i in nums[3:]:
+        if i > f:
+            t=s
+            s=f
+            f=i
+        elif i>s:
+            t=s
+            s=i
+        elif i>t:
+            t=i
+    return t
 print(thirdMax([1,2,5,6,7]))
